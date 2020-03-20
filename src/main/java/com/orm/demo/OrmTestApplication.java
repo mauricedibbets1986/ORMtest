ee07951e4f265a665172d6bd5cbea56f39cbaa1b
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-03-20 10:38:50 +0100

    start ORM opdrachten in main uitgetypt

--- src/main/java/com/orm/demo/OrmTestApplication.java
+++ src/main/java/com/orm/demo/OrmTestApplication.java
@@ -1,5 +1,7 @@
 package com.orm.demo;
 
+import javax.persistence.Entity;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,3 +13,34 @@ public class OrmTestApplication {
 	}
 
 }
+
+/*
+ * opzet ORM test:
+ * ManyToOne OneToMany -> Meerdere medewerkers 1 afdeling
+ * OneToOne -> 1 medewerker 1 telefoon
+ * ManyToMany -> Meerdere medewerkers met meerdere opdrachten
+ */
+
+/* 
+ * Wat Moet de app kunnen?
+ * ManyToOne stappen:
+ * stap 1 Medewerker aanmaken in html,js html
+ * 			nodig:
+ * 			medewerker @Entity
+ * 			medewerker repository
+ * 			medewerker service
+ * 			medewerker endpoint
+ * 			HTML form richting endpoint
+ * stap 2 Afdeling aanmaken in html,js
+ * stap 3 Bestaande medewerker koppelen aan bestaande afdeling in html,js
+ * stap 4 Nieuwe medewerker direct koppelen aan bestaande afdeling in html,js
+ * 
+ * OneToOne stappen:
+ * stap 1 Telefoon aanmaken in html,js
+ * stap 2 Bestaande Medewerker koppen aan bestaande telefoon html,js
+ * stap 3 Nieuwe medewerker aanmaken met een nieuwe telefoon in 1 form
+ * 
+ * ManyToMany stappen:
+ * stap 1 Opdrachten aanmaken html,js
+ * stap 2 Opdrachten koppelen aan Medewerkers
+ */
