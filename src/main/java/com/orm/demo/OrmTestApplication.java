a2c614647b99e24111b6a05ffaa8adbd1e8c1490
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-03-20 10:25:24 +0100

    start ORM

---
+++ src/main/java/com/orm/demo/OrmTestApplication.java
@@ -0,0 +1,13 @@
+package com.orm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmTestApplication.class, args);
+	}
+
+}
